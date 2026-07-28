92f4d6eb86ead41781dd4e2d44824025f40a90a6
Sam C <chuquest.work@gmail.com>
2026-07-28 14:30:21 +0100

    Register code integration

--- api/index.js
+++ api/index.js
@@ -156,12 +156,12 @@ app.post('/api/login', async (req, res) => {
             }
         }
 
-        res.status(200).json(sendResponse("200", "Login Successful", msg));
+        res.status(200).json(sendResponse("200", "Login Successful"));
         await mongoose.disconnect();
 
     } catch (error) {
         // console.error('Login error:', error);
-        res.status(500).json(sendResponse("500", "Error processing, try again", ""));
+        res.status(500).json(sendResponse("500", "Error processing, try again: "+error));
     }
 });
 
