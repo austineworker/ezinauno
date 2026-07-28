3c21f4c82a1ba9b2aababe2e016b06c8eafaf85f
Sam C <chuquest.work@gmail.com>
2026-07-28 21:05:47 +0100

    Testing newpustat

--- api/index.js
+++ api/index.js
@@ -804,15 +804,21 @@ app.post('/api/nwepustat', async(req, res) => {
                 let matu = new Date(data.matu).getTime();
                 let present = Data.now();
 
-                if (present > matu) {
-                    //matured already
-                    let everyData = {
-                        udiEgo: uzoUgwo,
-                        egoOne: currentEgoOne,
-                        pendEgo: nweputa
-                    }
-                    allData.push(everyData);
+                let everyData = {
+                    matu: matu,
+                    present: present
                 }
+                allData.push(everyData);
+
+                // if (present > matu) {
+                //     //matured already
+                //     let everyData = {
+                //         udiEgo: uzoUgwo,
+                //         egoOne: currentEgoOne,
+                //         pendEgo: nweputa
+                //     }
+                //     allData.push(everyData);
+                // }
             });
 
             res.status(200).json(sendResponse("200", allData));
