572004169eea7b7685ce2dd1eeb57514a80af3bb
Sam C <chuquest.work@gmail.com>
2026-07-29 14:39:18 +0100

    Nwepustat debug 6

--- api/index.js
+++ api/index.js
@@ -822,7 +822,7 @@ app.post('/api/nwepustat', async(req, res) => {
                 //         pendEgo: nweputa
                 //     }
                 // }
-                res.status(200).json(sendResponse("200", ntinyeData?.nweputa));
+                res.status(200).json(sendResponse("200", ntinyeData));
             }
             else if (ntinyeData?.length > 1) {
                 let allData = [];
