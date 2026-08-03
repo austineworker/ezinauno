6263adf5bfc1227af834e97743a208ec1244cb76
Sam C <chuquest.work@gmail.com>
2026-08-03 20:30:21 +0100

    Cors allowed urls edited

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const app = express();
 // Middleware
 app.use(bodyParser.json());
 
-const allowedOrigins = ['https://iruezinauno.vercel.app','https://glassdoorholding.org', 'http://localhost:5173', 'http://localhost:5174', 'https://n08.vercel.app', 'https://fgcn08.com'];
+const allowedOrigins = ['https://iruezinauno.vercel.app','https://glassdoorholding.org', 'http://localhost:5173', 'http://localhost:5174'];
 app.use(cors({
     origin: (origin, callback) => {
         if (allowedOrigins.includes(origin) || !origin) {
